c93bb25da05bfbaf20987a3a5833ada5084aa73c
Red-Kyurem <50855216+Red-Kyurem@users.noreply.github.com>
2023-04-09 19:28:58 -0400

    (Finally) added button flashing when pressed

--- Assets/Scripts/ButtonAddIngredient.cs
+++ Assets/Scripts/ButtonAddIngredient.cs
@@ -6,16 +6,19 @@ public class ButtonAddIngredient : MonoBehaviour
 {
     public string IngredientString = "";
     public GameObject cup;
+    private Color buttonColor;
+    public Color pressedButtonColor = new Color(1, 0.33f, 0.33f);
+    GameObject tCoffeePour;
     // Start is called before the first frame update
     void Start()
     {
-        
+        buttonColor = GetComponent<Renderer>().material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void AddIngredient()
     {
@@ -23,9 +26,12 @@ public class ButtonAddIngredient : MonoBehaviour
         if (cup != null && cup.GetComponent<PickupableObject>().objectType == ObjectType.Cup)
         {
             cup.GetComponent<CupContents>().ingredientStrings.Add(IngredientString);
+            tCoffeePour.SetActive(true);
+            GetComponent<Renderer>().material.color = pressedButtonColor;
+            Invoke("ResetColor", 0.25f);
 
             // ADD TELEMETRY HERE (IngredientAdded)
-            string cupContentsCombinedString = string.Join(", ",cup.GetComponent<CupContents>().ingredientStrings);
+            string cupContentsCombinedString = string.Join(", ", cup.GetComponent<CupContents>().ingredientStrings);
 
             var data = new TelemetryStructs.ingredientAddedData()
             {
@@ -37,5 +43,10 @@ public class ButtonAddIngredient : MonoBehaviour
 
         }
 
+    }
+    public void ResetColor()
+    {
+        GetComponent<Renderer>().material.color = buttonColor;
+        tCoffeePour.SetActive(false);
     }
 }
